ba03866707770fbea41a99fe5b6c0173bb5cc9aa
Linas Kondrackis <linaskondrackis@gmail.com>
2018-09-25 23:09:46 +0100

    Auto indent files to be worked on

--- src/tools.cpp
+++ src/tools.cpp
@@ -10,16 +10,16 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
-}
+    const vector<VectorXd> &ground_truth) {
+        /**
+        TODO:
+        * Calculate the RMSE here.
+        */
+    }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
-}
+    MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+        /**
+        TODO:
+        * Calculate a Jacobian here.
+        */
+    }
